a5164b19c12fbdb7c320b3d628a0b5a4b2d6a071
Eli Geske <eligeske@gmail.com>
2017-10-10 14:26:19 -0400

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my-cloudfiles-php5.3-wrapper
-wraps rackspaces opencloud php library hard set to php 5.3 supporting version 
+wraps rackspaces opencloud php library hard set to php 5.3 supporting version
+
+Wrapper around PHP 5.3 stable version of rackspace php-opencloud
+- new packages have min version of 5.4
+- read more http://docs.php-opencloud.com/en/latest/using-php-5.3.html
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* Wrapper around PHP 5.3 stable version of rackspace php-opencloud
+* - new packages have min version of 5.4
+* - read more http://docs.php-opencloud.com/en/latest/using-php-5.3.html
+*/
+
+require 'vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+use OpenCloud\ObjectStore\Resource\DataObject;
+
+class MyCloudFilesAdmin {
+
+	private $service;
+
+	/**
+	* $settings = array(
+	* 	'username' => 'someUserName',
+	* 	'apiKey' => 'asdfasfdafdaf',
+	* 	'region' => 'IAD'
+	* );
+	*
+	*/
+	public function __construct($settings) {
+		$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+			'username' => $settings['username'],
+			'apiKey' => $settings['apiKey']
+		));
+		$this->service = $client->objectStoreService(null, $settings['region']);
+	}
+
+	public function getContainerList() {
+		return $this->service->listContainers();
+	}
+
+	public function getContainerObjectList($containerName) {
+		// http://docs.php-opencloud.com/en/latest/services/object-store/objects.html#list-objects-in-a-container
+		$container = $this->service->getContainer($containerName);
+		return $container->objectList();
+	}
+
+	/**
+	* Uploads a file with an assigned name to container
+	* - http://docs.php-opencloud.com/en/latest/services/object-store/objects.html#upload-a-single-file-under-5gb-with-metadata
+	*/
+	public function createFileObject($containerName, $fileName, $filePathAndName, $contentType) {
+		$container = $this->service->getContainer($containerName);
+		$container->uploadObject($fileName, fopen($filePathAndName, 'r+'), array('Content-Type' => $contentType));
+	}
+	public function createFileObjectFromString($containerName, $fileName, $fileString, $contentType) {
+		$container = $this->service->getContainer($containerName);
+		$container->uploadObject($fileName, $fileString, array('Content-Type' => $contentType));
+	}
+
+}
